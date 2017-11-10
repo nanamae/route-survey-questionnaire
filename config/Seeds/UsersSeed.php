fb74eacf7b37662a264dafd5a207a6678ec4a7b3
nanamae <naaaats45@gmail.com>
2017-11-10 07:19:44 +0000

    seeder

---
+++ config/Seeds/UsersSeed.php
@@ -0,0 +1,35 @@
+<?php
+use Migrations\AbstractSeed;
+use Cake\Auth\DefaultPasswordHasher;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $hasher = new DefaultPasswordHasher();
+        $data = [
+                 [
+                    'username' => 'nanamae2',
+                    'password' => $hasher->hash('test'), 
+                    'role' => 'admin'
+                ]
+            ];
+        
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
